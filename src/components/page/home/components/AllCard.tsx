40a7aa7f335ecff1187af24ccded0f0be4edd711
JeYeongR <wpdud2003@gmail.com>
2025-06-14 00:14:32 +0900

    feat: 썸네일 이미지 URL이 없을 경우 기본 이미지 설정

--- src/components/page/home/components/AllCard.tsx
+++ src/components/page/home/components/AllCard.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import Image from "next/image";
-import { useState } from "react";
 import CardBox from "@components/_utiles/card/CardBox.component";
 import Pagination from "@components/_utiles/pagination/pagination.component";
+import Image from "next/image";
+import { useState } from "react";
 import styles from "./AllCard.module.scss";
 
 interface Post {
@@ -59,7 +59,7 @@ export default function AllCardComponent({ type, posts, itemsPerPage = 20 }: Pos
                                         ? `/post/view/${item.id}?type=company`
                                         : `/post/view/${item.id}?type=member`
                                 }
-                                imageSrc={item.thumbnailImageUrl}
+                                imageSrc={item.thumbnailImageUrl ?? 'https://cdn.threed.site/thumbnails/threed.png'}
                                 isNew={item.isNew}
                                 isHot={item.isHot}
                                 title={item.title}
