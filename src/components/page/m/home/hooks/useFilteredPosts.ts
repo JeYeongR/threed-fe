86b8dc820871a3f7b545974a8cf561b9a2317604
JeYeongR <wpdud2003@gmail.com>
2025-06-13 10:44:03 +0900

    feat: 게시물 검색 API 호출 시 페이지네이션 파라미터 추가

--- src/components/page/m/home/hooks/useFilteredPosts.ts
+++ src/components/page/m/home/hooks/useFilteredPosts.ts
@@ -64,7 +64,7 @@ export async function fetchPosts(
 
         // API 요청 수행
         const response = await api.get<PostResponse>(
-            `/api/v1/${type}-posts/search?${queryString.toString()}`
+            `/api/v1/${type}-posts/search?${queryString.toString()}&page=1&size=999999`
         );
         return response.elements || [];
 
